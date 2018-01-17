abf39a7175f9e3ea253edcc2becfd733181cb018
root <root@DESKTOP-FJTKO68.localdomain>
2018-01-18 02:46:08 +0300

    Adding timeseries

--- src/components/BasicTabsWrappedLabel.js
+++ src/components/BasicTabsWrappedLabel.js
@@ -17,10 +17,109 @@ import SessionTable from './SessionTable'
 
 import UpdateButton from './UpdateButton'
 
+//http://bda01-t01-hsd02.lab.nordigy.ru:3000/public/question/15a3a3b0-b5e3-4cb9-97bf-888c977959aa
+const test_data_timelines = {
+
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+const shade = 0.9166666666666666
+
+//bs
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
+//bs
+function numberToColorHsl(i, min, max) {
+    var ratio = i;
+    if (min> 0 || max < 1) {
+        if (i < min) {
+            ratio = 0;
+        } else if (i > max) {
+            ratio = 1;
+        } else {
+            var range = max - min;
+            ratio = (i-min) / range;
+        }
+    }
+
+    // as the function expects a value between 0 and 1, and red = 0° and green = 120°
+    // we convert the input to the appropriate hue value
+    var hue = ratio * 1.2 / 3.60;
+    //if (minMaxFactor!=1) hue /= minMaxFactor;
+    //console.log(hue);
+
+    // we convert hsl to rgb (saturation 100%, lightness 50%)
+    var rgb = hslToRgb(hue, 1, .5);
+    // we format to css value and return
+    //return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+    return "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
+}
+
+//alert(numberToColorHsl(shade,0,1))
+
+
 
 const dataTimelines = {
       rows: [
-      [ 'John',  'Desktop Apppp', '#32CD32',   new Date(0,0,0,16,30,0),  new Date(0,0,0,19,0,0) ],
+      [ 'Loading',  'Loading', numberToColorHsl(0.9166666666666666,0,1),   new Date(1514277004106),  new Date(1514277064112) ],
+    ],
+    columns: [
+      {
+        "id": "Name",
+        "type": "string"
+      },
+      {
+        "id": "Session ID",
+        "type": "string"
+      },
+      {
+        type: 'string',
+        role: 'style'
+      },
+      {
+        "id":"Start",
+        "type":"date"
+      },
+      {
+        "id":"End",
+        "type":"date"
+      }
+    ]};
+
+
+const dataTimelines2 = {
+      rows: [
+      [ 'John',  'Desktop Apppp',  numberToColorHsl(shade,0,1) ,   new Date(0,0,0,16,30,0),  new Date(0,0,0,19,0,0) ],
       [ 'John',  'Desktop App', '#FF3E33',   new Date(0,0,0,19,0,0),  new Date(0,0,0,23,0,0) ],
       [ 'John1',  'Mobile App - iOS','#32CD32', new Date(0,0,0,16,30,0), new Date(0,0,0,19,0,0) ],
       [ 'Claire', 'Desktop App', '#FF3E33',  new Date(0,0,0,13,30,0), new Date(0,0,0,14,3,0) ],
@@ -173,7 +272,7 @@ class BasicTabsWrappedLabel extends React.Component {
         </TabContainer>}
         {value === 'two' && <TabContainer>
           <div>
-            <TimelineChart rows={dataTimelines.rows} columns={dataTimelines.columns}/>
+            <TimelineChart hub_id={hub_id} rows={dataTimelines.rows} columns={dataTimelines.columns}/>
             <SessionTable hub_id={hub_id} changeTabHub={this.changeTab}/>
           </div>
         </TabContainer>}
